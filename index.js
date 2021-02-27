17f6525a45c2dcac9e9d2de4b5695bacd6e96499
Grant Sykes <grantrjsykes@googlemail.com>
2021-02-27 15:48:18 +0000

    Build progress

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 
-
-export const flatten = x => x.flat();
-
-
-
-export const map = x => y => y.map(x);
-
-
+export const curry = func => {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function (...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }
+    };
+};
 
 export const pipe = (...fns) => (x) => fns.reduce((acc, curr) => curr(acc), x);
 
+export const add = curry((x, y) => x + y);
 
+export const divide = curry((x, y) => y / x);
+
+export const flatten = x => x.flat();
+
+export const map = x => y => y.map(x);
+
+export const multiply = curry((x, y) => x * y);
 
 export const reverse = x => x.reverse();
 
-
-
 export const self = x => x;
-
-
 
 export const sort = x => x.sort();
 
+export const subtract = curry((x, y) => y - x);
 
+export const sum = x => x.reduce(add, 0);
 
 export const unique = x => Array.from(new Set(x));
 
-
-
-
-
-
 export default {
+    add,
     flatten,
     map,
+    multiply,
     pipe,
     reverse,
     self,
     sort,
+    subtract,
+    sum,
     unique,
 };
--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,34 @@
-
 import { 
+    add,
+    divide,
     flatten,
     map,
+    multiply,
     pipe,
     reverse,
     self,
     sort,
+    subtract,
+    sum,
     unique
 } from './';
+
+describe('add', () => {
+    test('it adds two numbers', () => {
+        expect(add(3, 4)).toEqual(7);
+        expect(add(5)(6)).toEqual(11);
+    });
+});
+
+describe('divide', () => {
+    test('it divides the second argument by the first and returns the result', () => {
+        expect(divide(3, 12)).toEqual(4);
+        expect(divide(1)(20)).toEqual(20);
+        expect(divide(4)(3)).toEqual(0.75);
+        expect(divide(0)(0)).toEqual(NaN);
+        expect(divide(0)(5)).toEqual(Infinity);
+    });
+});
 
 describe('flatten', () => {
     test('it returns a flattened array', () => {
@@ -40,7 +61,6 @@ describe('pipe', () => {
     });
 });
 
-
 describe('map', () => {
     test('it iterates over an array', () => {
         const data = [1, 2, 3, 4, 5, 6];
@@ -56,6 +76,18 @@ describe('map', () => {
             map(x => x + 1)
         )(data);
         expect(result).toEqual([2, 3, 4, 5, 6, 7]);
+    });
+});
+
+describe('multiply', () => {
+    test('it multplies two arguments and returns the result', () => {
+        expect(multiply(1, 1)).toEqual(1);
+        expect(multiply(1, 0)).toEqual(0);
+        expect(multiply(5, 5)).toEqual(25);
+        expect(multiply(3, 4)).toEqual(12);
+        expect(multiply(4, 3)).toEqual(12);
+        expect(multiply(6)(6)).toEqual(36);
+        expect(multiply(100)(10)).toEqual(1000);
     });
 });
 
@@ -87,12 +119,34 @@ describe('sort', () => {
     });
 });
 
+describe('subtract', () => {
+    test('it subtracts the first argument from the second and returns the result', () => {
+        expect(subtract(7, 9)).toEqual(2);
+        expect(subtract(5)(2)).toEqual(-3);
+        expect(subtract(0)(15)).toEqual(15);
+        expect(subtract(0)(0)).toEqual(0);
+        expect(subtract(25)(25)).toEqual(0);
+    });
+});
+
+describe('sum', () => {
+    test('it adds together an array of numbers (of any length) and returns the result', () => {
+        expect(sum([1, 2, 3])).toEqual(6);
+        expect(sum([1, 2, 3, 4, 5, 6])).toEqual(21);
+        expect(sum([0])).toEqual(0);
+        expect(sum([0, 1])).toEqual(1);
+        expect(sum([5, -3, -1])).toEqual(1);
+        expect(sum([])).toEqual(0);
+    });
+});
+
 describe('unique', () => {
     test('it returns an array unchanged if empty', () => {
         expect(unique([])).toEqual([]);
     });
 
     test('it returns an array of numbers with all duplicates removed', () => {
+        expect(unique([1])).toEqual([1]);
         expect(unique([1, 2, 2, 3, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
     });
 
